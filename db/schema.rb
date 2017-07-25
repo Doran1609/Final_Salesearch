b7ed37dec5fd9649a8d15ad1edbc92de9751c821
Dorancelis Cordones <doran162@hotmail.com>
2017-07-25 22:31:56 +0000

    All project with admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724195341) do
+ActiveRecord::Schema.define(version: 20170725222753) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
